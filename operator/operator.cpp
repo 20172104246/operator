ed0257049c54c898718c79fda3a7160d1f843db3
20172104246 <1726857237@qq.com>
2018-05-28 16:59:30 +0800

    增加了unplay函数以及更改了一些函数名字
    1.+unplay()
    2.display1()-display()
    3.+心型图案

--- operator/operator.cpp
+++ operator/operator.cpp
@@ -12,18 +12,18 @@ private:
 	int inches;
 public:
 	void setvaluve(int fe,int in);
-	void display1();
-	//void display2();
+	void display();
+	void unplay(int a,double b);
 	Ckobe operator+(Ckobe & wtf);
 };
 
 void Ckobe::setvaluve(int fe,int in)
 {
 	feet = fe + in / 12;
-	inches = in + in % 12;
+	inches = in % 12;
 }
 
-void Ckobe::display1()
+void Ckobe::display()
 {
 	cout << feet << "英尺" << inches << "英寸" << endl;
 }
@@ -35,17 +35,44 @@ Ckobe Ckobe::operator+(Ckobe & wtf)
 	return temp;
 }
 
+void Ckobe::unplay(int a,double b)
+{
+	feet = feet + a;
+	if (inches + b > 12)
+	{
+		feet = feet + 1;
+		inches = (inches + b) - 12;
+		cout << feet << "英尺" << inches << "英寸" << endl;
+		float q, x, y;
+		for (y = 1.5f; y>-1.5f; y -= 0.1f)
+		{
+			for (x = -1.5f; x<1.5f; x += 0.05f)
+			{
+				q = x*x + y*y - 1;
+				//这里的@符号即为打印出的心形图案符号，可更改  
+				char ch = q*q*q - x*x*y*y*y <= 0.0f ? '@' : ' ';
+				putchar(ch);
+				//cout<<"或者putchar(q*q*q-x*x*y*y*y<=0.0f?'*':' ')";  
+			}
+			cout << "\n" << endl; //printf("\n");
+		}
+
+	}
+}
 
 
 int main()
 {
 	Ckobe A, B, C;
 	A.setvaluve(5, 10);
-	A.display1();
+	A.display();
+	A.unplay(5, 10);
 	B.setvaluve(5, 9);
-	B.display1();
+	B.display();
+	B.unplay(5, 9);
 	C = A + B;
-	C.display1();
+	C.display();
+	C.unplay(5, 9);
     return 0;
 }
 
